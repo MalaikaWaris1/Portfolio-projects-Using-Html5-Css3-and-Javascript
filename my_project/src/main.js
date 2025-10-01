792cba2b579c9b250218ab12f2b524fc795d7312
malaikapad <ahmedcric09@gmail.com>
2025-10-01 18:50:03 +0500

    Add files via upload

---
+++ my_project/src/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+import products from './Api/product.json';
+import { showProductContainer } from '../homeproductcard';
+
+// console.log(products);
+
+showProductContainer(products);   //press ctrl + space automaticlly sujjest you
